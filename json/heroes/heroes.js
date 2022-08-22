eb1d2b7d1c79d8b1d683b2ddb970ce56b03df51f
DollayaDollaya <107751894+DollayaDollaya@users.noreply.github.com>
2022-08-22 17:46:43 -0400

    Update heroes.js

--- json/heroes/heroes.js
+++ json/heroes/heroes.js
@@ -9,3 +9,5 @@ const section = document.querySelector('section');
 let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 
 let request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
